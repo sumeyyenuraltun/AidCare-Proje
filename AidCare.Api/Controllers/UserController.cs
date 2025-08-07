f92fbf53baa5e8b135d53efabe6b3e1470825044
sumeyyenur <sumeyyenur2004@gmail.com>
2025-08-07 22:24:46 +0300

    UserController'ı yazıldı

---
+++ AidCare.Api/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using AidCare.Business.Abstract;
+using AidCare.Entities.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AidCare.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user)
+        {
+            _userService.Add(user);
+            return Ok(user);
+
+        }
+    }
+}
--- AidCare.Api/Program.cs
+++ AidCare.Api/Program.cs
@@ -1,4 +1,8 @@
+using AidCare.Business.Abstract;
+using AidCare.Business.Concrete;
+using AidCare.DataAccess.Abtract;
 using AidCare.DataAccess.Concrete.Context;
+using AidCare.DataAccess1.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 builder.Services.AddDbContext<AidCareDbContext>(option=>option.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<IUserService, UserManager>();
+builder.Services.AddScoped<IUserDAL, UserRepository>();
 
 builder.Services.AddOpenApi();
 
